93dfcf5aefc49cc1502bd18f18e55d18a72e3dc2
Sahil Bansal <shlbnsl843@gmail.com>
2018-05-15 22:59:15 +0530

    solved A and B, C was more mathematical

---
+++ Codeforces/Div 2 Rounds/483 Div 2/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector <int> vi;
+typedef pair <int,int> pii;
+typedef long long ll;
+typedef unsigned long long ull;
+#define fl(i,a,b) for(int i(a);i<(b);i++)
+#define rep(i,n) fl(i,0,n)
+#define rfl(i,a,b) for(int i(a);i>=(b);i--)
+#define srt(v) sort((v).begin(),(v).end())
+#define pb push_back
+#define mp make_pair
+#define MOD 1000000007
+#define slen(s) s.length()
+#define F first
+#define S second
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif 	
+
+	int n, x;
+
+	vi a;
+
+	cin >> n; 
+
+	rep(i, n) {
+		cin >> x;
+		a.pb(x);
+	}
+
+	srt(a);
+
+	cout << a[(n - 1)/2];
+	return 0;
+}
