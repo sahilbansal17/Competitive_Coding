86876cb9ff2803893cf0dd33ed9bf15d095ca0bc
Sahil <shlbnsl843@gmail.com>
2018-06-05 20:26:17 +0530

    Add template file in C++

---
+++ Template/template.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Template file for Online Algorithmic Competitions (Lines 6 - 33) */
+/* Typedefs */
+	/* Basic types */
+	typedef long long ll;
+	typedef unsigned long long ull;
+	/* STL containers */
+	typedef vector <int> vi;
+	typedef vector <ll> vll;
+	typedef pair <int,int> pii;
+	typedef pair <ll,ll> pll;
+	typedef vector < pii > vpii;
+	typedef vector < pll > vpll;
+/* Macros */
+	/* Loops */
+	#define fl(i,a,b) for(int i(a);i<(b);i++)
+	#define rep(i,n) fl(i,0,n)
+	#define rfl(i,a,b) for(int i(a);i>=(b);i--)
+	/* Algorithmic functions */
+	#define srt(v) sort((v).begin(),(v).end())
+	/* STL container methods */
+	#define pb push_back
+	#define mp make_pair
+	/* String methods */
+	#define slen(s) s.length()
+	/* Shorthand notations */
+	#define F first
+	#define S second
+	#define MOD 1000000007
+	#define MAX 100010
+
+/* Global variable declarations */
+
+
+
+
+
+
+
+
+
+int main(){
+
+	/* Input & Output from files for easy testing (Lines 54 - 57) 
+	 * These lines do not affect the submission on Online Judges.
+	 * Comment them if input is to be taken from stdin and 
+	 * output is to be given to stdout. 
+	 * If input and output is to be taken from file, then:
+	 * Change the input file address as the first parameter of freopen on line 55.
+	 * Change the output file address as the first parameter of freopen on line 56. */
+	#ifndef ONLINE_JUDGE
+	freopen("/Users/sahilbansal/Desktop/input.txt","r",stdin);
+	freopen("/Users/sahilbansal/Desktop/output.txt","w",stdout);
+	#endif
+	
+
+	
+	return 0;
+}
+
+/* Submitted -
+ * on Date: 
+ * at Time: 
+ * Problem Link -
+ */
