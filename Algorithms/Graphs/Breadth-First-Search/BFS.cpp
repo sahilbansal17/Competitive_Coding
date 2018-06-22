a1a1faff4bb6ff275e05047f2f75d95f937fd5d8
mohit <mjain8156@gmail.com>
2018-06-23 02:11:49 +0530

    Add Breadth First Search Algorithm

---
+++ Algorithms/Graphs/Breadth-First-Search/BFS.cpp
@@ -0,0 +1,74 @@
+// Compile in C++11 or higher
+#include <iostream>
+#include <map>
+#include <list>
+#include <queue>
+#include <climits>
+using namespace std;
+
+template<typename T>
+class Graph{
+    map<T,list<T> > adjList;
+public:
+       Graph(){}
+       void addEdge(T u,T v,bool bidir = true){
+       adjList[u].push_back(v);
+       if(bidir){
+        adjList[v].push_back(u);
+       }
+       }
+       void print(){
+       //Iteration
+       for(auto i:adjList){
+        cout<<i.first<<"->";
+        //i.second is LL
+        for(T entry:i.second){
+            cout<<entry<<",";
+        }
+        cout<<endl;
+       }
+       }
+       void bfs(T src){
+        queue<T> q;
+        map<T,int>dist;
+        map<T,T>parent;
+        for(auto i:adjList){
+            dist[i.first] = INT_MAX;
+        }
+        q.push(src);
+        dist[src] = 0;
+        parent[src] = src;
+        while(!q.empty()){
+            T node = q.front();
+            cout<<node<<" ";
+            q.pop();
+            //For the neighbours of the current node
+
+         for(int neighbour : adjList[node]){
+            if(dist[neighbour] == INT_MAX){
+                q.push(neighbour);
+                dist[neighbour] = dist[node] +1;
+                parent[neighbour] = node;
+            }
+         }
+
+    }
+     //Print the distance to all the nodes
+       for(auto i:adjList){
+        T node = i.first;
+        cout<<"Dist of"<<node<<"from"<<src<<"is"<<dist[node]<<endl;
+       }
+       }
+};
+int main(){
+Graph<int> g;
+g.addEdge(0,1);
+g.addEdge(1,2);
+g.addEdge(0,4);
+g.addEdge(2,4);
+g.addEdge(2,3);
+g.addEdge(3,5);
+g.addEdge(3,4);
+g.bfs(0);
+return 0;
+}
