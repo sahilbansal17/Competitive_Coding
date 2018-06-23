a25a8407766bc9c82e7c944ddb0c7099be028683
mohit <mjain8156@gmail.com>
2018-06-23 21:34:49 +0530

    Modification in Topological-sorting

--- Algorithms/Graphs/Topological-sorting/topological-sorting.cpp
+++ Algorithms/Graphs/Topological-sorting/topological-sorting.cpp
@@ -8,76 +8,96 @@
 using namespace std;
 
 template<typename T>
-class Graph{
+class Graph
+{
     map<T,list<T> > adjList;
 public:
-       Graph(){}
-       void addEdge(T u,T v,bool bidir = true){
-       adjList[u].push_back(v);
-       if(bidir){
-        adjList[v].push_back(u);
-       }
-       }
-       void print(){
-       //Iteration
-       for(auto i:adjList){
-        cout<<i.first<<"->";
-        //i.second is LL
-        for(T entry:i.second){
-            cout<<entry<<",";
+    Graph() {}
+    void addEdge(T u,T v,bool bidir = true)
+    {
+        adjList[u].push_back(v);
+        if(bidir)
+        {
+            adjList[v].push_back(u);
         }
-        cout<<endl;
-       }
-       }
+    }
+    void print()
+    {
+        //Iteration
+        for(auto i:adjList)
+        {
+            cout<<i.first<<"->";
+            //i.second is LL
+            for(T entry:i.second)
+            {
+                cout<<entry<<",";
+            }
+            cout<<endl;
+        }
+    }
 
-       void topologicalSort(){
-       queue<T> q;
-       map<T,bool> visited;
-       map<T,int> indegree;
-       for(auto i:adjList){
-        T node = i.first;
-        visited[node] = false;
-        indegree[node] = 0;
-       }
-       // Initialize indegrees
-       for(auto i: adjList){
-        T u = i.first;
-        for(T v : adjList[u]){
-            indegree[v]++;
+    void topologicalSort()
+    {
+        queue<T> q;
+        map<T,bool> visited;
+        map<T,int> indegree;
+        for(auto i:adjList)
+        {
+            T node = i.first;
+            visited[node] = false;
+            indegree[node] = 0;
         }
-       }
-       //Find nodes with 0 indegree
-       for(auto i:adjList){
-        T node = i.first;
-        if(indegree[node] ==0){
-            q.push(node);
-        }
-       }
-       //Start with algorithm
-       while(!q.empty()){
-        T node = q.front();
-        q.pop();
-        cout<<node<<"->";
-        for(T neighbour:adjList[node]){
-            indegree[neighbour]--;
-            if(indegree[neighbour]==0){
-                    q.push(neighbour);
-
+        // Initialize indegrees
+        for(auto i: adjList)
+        {
+            T u = i.first;
+            for(T v : adjList[u])
+            {
+                indegree[v]++;
             }
         }
-       }
-       }
+        //Find nodes with 0 indegree
+        for(auto i:adjList)
+        {
+            T node = i.first;
+            if(indegree[node] ==0)
+            {
+                q.push(node);
+            }
+        }
+        //Start with algorithm
+        while(!q.empty())
+        {
+            T node = q.front();
+            q.pop();
+            cout<<node<<"->";
+            for(T neighbour:adjList[node])
+            {
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0)
+                {
+                    q.push(neighbour);
+
+                }
+            }
+        }
+    }
 
 };
-int main(){
-Graph<string> g;
-g.addEdge("English","ProgrammingLogic",false);
-g.addEdge("Maths","ProgrammingLogic",false);
-g.addEdge("ProgrammingLogic","HTML",false);
-g.addEdge("ProgrammingLogic","python",false);
-g.addEdge("ProgrammingLogic","Java",false);
-g.addEdge("ProgrammingLogic","JS",false);
-g.addEdge("Python","WebDev",false);
-g.addEdge("HTML","CSS",false);
-g.addEdge("JS","WebDev",false);
-return 0;}
+int main()
+{
+    Graph<string> g;
+    g.addEdge("English","ProgrammingLogic",false);
+    g.addEdge("Maths","ProgrammingLogic",false);
+    g.addEdge("ProgrammingLogic","HTML",false);
+    g.addEdge("ProgrammingLogic","python",false);
+    g.addEdge("ProgrammingLogic","Java",false);
+    g.addEdge("ProgrammingLogic","JS",false);
+    g.addEdge("Python","WebDev",false);
+    g.addEdge("HTML","CSS",false);
+    g.addEdge("JS","WebDev",false);
+    g.print();
+    g.topologicalSort();
+    return 0;
+}
+// Time Complexity of above code is O(V+E)
