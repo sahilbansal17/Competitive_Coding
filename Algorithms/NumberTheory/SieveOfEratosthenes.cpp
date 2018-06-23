0e74a114df614a49c2d0d7a08095299a338775b8
Lavish Saluja <lavishsaluja.ls@gmail.com>
2018-06-24 01:11:56 +0530

    Update SieveOfEratosthenes.cpp

--- Algorithms/NumberTheory/SieveOfEratosthenes.cpp
+++ Algorithms/NumberTheory/SieveOfEratosthenes.cpp
@@ -7,30 +7,30 @@ using namespace std;
 int main(void){
     int n;
     cin>>n;
-    //prime[i]=1 will imply that i is prime & prime[i]=0 will imply that i is not prime
+    //prime[i] = 1 will imply that i is prime & prime[i]=0 will imply that i is not prime
     int prime[n+1];
     for(int i = 0; i <= n; i ++)
 	//considering all as primes as of now.
-        prime[i]=1;
+        prime[i] = 1;
     //since 0 & 1 are not prime.
-    prime[0]=0;
-    prime[1]=0;
+    prime[0] = 0;
+    prime[1] = 0;
     /*
     Here we will be running are first loop only till sqrt(n) instead of n because we know that
     if a number is prime it does not have any factor till its square root so we
     can use it for optimization.
     */
-    for(int i=2;i<=sqrt(n);i++){
+    for(int i = 2; i <= sqrt(n);i ++){
 	//if i is prime
-        if(prime[i]==1){
-            for(int j=2;j*i<=n;j++){
+        if(prime[i] == 1){
+            for(int j = 2; j*i <= n; j ++){
 		//setting the multiples of i to 0 because they can't be prime.
-                prime[i*j]=0;
+                prime[i*j] = 0;
 	    }
         }
     }
-    for(int i=0;i<=n;i++){
-        if(prime[i]!=0){
+    for(int i = 0; i <= n; i ++){
+        if(prime[i] != 0){
             cout<<i<<endl;
         }
     }
