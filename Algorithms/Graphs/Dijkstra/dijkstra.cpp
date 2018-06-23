6c1aa6c33797da7b92f4fbbffdb6d5d4b54e306b
Sandip Kumar <sk1957339@gmail.com>
2018-06-24 00:02:34 +0530

    Dijkstra's Algorithm in c++
    here I try to use templates and basic method which are described using comments.

--- Algorithms/Graphs/Dijkstra/dijkstra.cpp
+++ Algorithms/Graphs/Dijkstra/dijkstra.cpp
@@ -1,81 +1,121 @@
-#include <cstdio>
-#include <cstdlib>
-#include <queue>
-#include <vector>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-#define MAXN 105
-#define INF 1<<30
+template<typename T>
+class Graph{
+    unordered_map<T, list<pair<T,int> > > m;
 
-typedef struct node{
-	int t, c;
-	node(){}
-	node(int T, int C) : t(T), c(C){}
-	bool operator < (const node &x) const{
-    return c < x.c;
-  }
-}No;
+public:
 
-vector<No> adj[MAXN];
-int mark[MAXN];
-int d[MAXN];
-priority_queue<No> pq;
+    void addEdge(T u,T v,int dist,bool bidir=true){
+        m[u].push_back(make_pair(v,dist));
+        if(bidir){
+            m[v].push_back(make_pair(u,dist));
+        }
+    }
+	
+    void printAdj(){
+        //Let try to print the adj list
+        //Iterate over all the key value pairs in the map
+        for(auto j:m){
 
-int dijkstra(int s, int t){
+            cout<<j.first<<"->";
 
-  for(int i = 0; i < MAXN; i++) {
-    mark[i] = 0;
-    d[i] = INF;
-  }
+            //Iterater over the list of cities
+            for(auto l: j.second){
+                    cout<<"("<<l.first<<","<<l.second<<")";
 
-	printf("%d %d", s, t);
+            }
+            cout<<endl;
+        }
+    }
+	
+    void dijsktraSSSP(T src){
 
-  pq.push(No(s,0));
+        unordered_map<T,int> dist;
 
-  while(!pq.empty()){
-    No top = pq.top(); pq.pop();
+        //Set all distance to infinity
+        for(auto j:m){
+            dist[j.first] = INT_MAX;
+        }
 
-    if(top.t == t){
-			return top.c;
+        //Make a set to find a out node with the minimum distance
+        set<pair<int, T> > s;
+
+        dist[src] = 0;
+        s.insert(make_pair(0,src));
+
+        while(!s.empty()){
+
+            //Find the pair at the front.
+            auto p =   *(s.begin());
+            T node = p.second;
+
+            int nodeDist = p.first;
+            s.erase(s.begin());
+
+
+            //Iterate over neighbours/children of the current node
+            for(auto childPair: m[node]){
+
+                if(nodeDist + childPair.second < dist[childPair.first]){
+
+
+                    //In the set updation of a particular is not possible
+                    // we have to remove the old pair, and insert the new pair to simulation updation
+                    T dest = childPair.first;
+                    auto f = s.find( make_pair(dist[dest],dest));
+                    if(f!=s.end()){
+                        s.erase(f);
+                    }
+
+                    //Insert the new pair
+                    dist[dest] = nodeDist + childPair.second;
+                    s.insert(make_pair(dist[dest],dest));
+
+                }
+
+            }
+        }
+        //Lets print distance to all other node from src
+        for(auto d:dist){
+
+            cout<<d.first<<" is located at distance of  "<<d.second<<endl;
+        }
+
+
     }
 
-    if(mark[top.t] == 1) continue;
-    mark[top.t] = 1;
-
-    for(int i = 0; i < adj[top.t].size(); i++){
-      int v = adj[top.t][i].t;
-      int cost = adj[top.t][i].c;
-
-      if (top.c + cost < d[v]){
-        d[v] = top.c + cost;
-        pq.push(No(v, top.c + cost));
-      }
-    }
-	}
-
-  return -1;
-}
+};
 
 int main(){
-  int T; // Number of edges
-	scanf("%d", &T);
-  //Nodes are numbered from 0 to MAXN-1
-  while(T-- > 0){
-      int a,b,c;
-      scanf("%d %d %d", &a, &b, &c);
-      adj[a].push_back(No(b,c)); //Unidirectional adj
-  }
 
-	int source, destination;
 
-	scanf("%d %d", &source, &destination);
 
-	int result = dijkstra(source, destination);
+    //Graph<int> g;
+    //g.addEdge(1,2,1);
+    //g.addEdge(1,3,4);
+    g.addEdge(2,3,1);
+    g.addEdge(3,4,2);
+    g.addEdge(1,4,7);
+    //g.printAdj();
+   // g.dijsktraSSSP(1);
 
-  if(result == -1){
-    printf("There is no path to destination\n");
-  }else{
-    printf("The path from %d to %d has cost %d\n", source, destination, result);
-  }
+
+
+
+    Graph<string> india;
+    india.addEdge("Amritsar","Delhi",1);
+    india.addEdge("Amritsar","Jaipur",4);
+    india.addEdge("Jaipur","Delhi",2);
+    india.addEdge("Jaipur","Mumbai",8);
+    india.addEdge("Bhopal","Agra",2);
+    india.addEdge("Mumbai","Bhopal",3);
+    india.addEdge("Agra","Delhi",1);
+    //india.printAdj();
+    india.dijsktraSSSP("Amritsar");
+
+
+
+ return 0;
 }
