f00ba7bb5b47dfbf2f13eae33a26a274e4c32523
shiv07tiwari <shivansh16tiwari@gmail.com>
2018-06-23 18:25:07 +0530

    New file for binary search and indentation improvement

--- Data Structures/Array Implementation/ArrayImplementation(Binary Search).cpp
+++ Data Structures/Array Implementation/ArrayImplementation(Binary Search).cpp
@@ -1,4 +1,5 @@
 //A programme for basic Array Implmentation - Binary Search. 
+
 #include <stdio.h>
 
   int binarysearch(int value, int left, int right, int array[]) {
@@ -19,20 +20,42 @@
 
     return -1; //Returns -1 if not found
   }
+void swap(int * x, int * y) {
+  int temp = * x; * x = * y; * y = temp;
+}
+
+// A function to implement bubble sort
+void Sort(int arr[], int n) {
+  int i, j;
+  for (i = 0; i < n - 1; i++) {
+   
+    for (j = 0; j < n - i - 1; j++)
+      if (arr[j] > arr[j + 1])
+        swap( & arr[j], & arr[j + 1]);
+  }
+}
 int main() {
   int array[100];
   int size, value;
-  printf("Enter the size of the sorted array : \n");
+  printf("Enter the size of the array : \n");
   scanf("%d", & size);
-  printf("Enter the values in a sorted increasing manner : \n");
+  printf("Enter the values : \n");
   for (int i = 0; i < size; i++) {
     scanf("%d", & array[i]);
   }
+  
+  Sort(array, size);//Array sorted so as to properly implement binary search. 
+  
   printf("Enter the value to search : \n");
   scanf("%d", & value);
+  
   int position = binarysearch(value, 0, size - 1, array);
+  printf("Sorted Array : \n");
+  for (int i = 0; i < size; i++)
+    printf("%d ", array[i]); //Printing the sorted array for reference.
+  printf("\n");
   if (position != -1)
-    printf("Value %d found at position %d \n", value, position + 1);
+    printf("Value %d found at position %d (In sorted array)\n", value, position + 1);
   else
     printf("Value not found \n");
 }
