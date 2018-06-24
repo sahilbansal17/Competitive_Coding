bc88f16892d4e90d4ac76e6fde9c2172c3974c6b
Anuj goyal <anujg935@gmail.com>
2018-06-24 21:13:01 +0530

    Fixed issue #57
    made some changes as suggested .

--- Data Structures/Linked-Lists/RevresingLinkedListTechniques.cpp
+++ Data Structures/Linked-Lists/RevresingLinkedListTechniques.cpp
@@ -1,13 +1,9 @@
 // Name: Anuj Goyal	
 // Email-id: anujg935@gmail.com
-
 // This program describes the 4 ways of revesing the linkedList
 //3 are the recursive ways and 1 is iterative way
-
 #include<iostream>
 using namespace std;
-
-
 //Node class defines the structure of linkedList
 class Node{
 	public:
@@ -18,8 +14,6 @@ class Node{
 			this->next=NULL;
 		}
 };
-
-
 //take inputs from the user until user enters -1
 //returns the head of the linkedList
 Node* takeInput(){
@@ -35,14 +29,12 @@ Node* takeInput(){
 		}	
 		else{
 			tail->next =n;
-			tail = tail->next;
-			
+			tail = tail->next;	
 		}	
 		cin>>data;
 	}
 	return head;
 } 
-
 //This function prints the linkedList
 void print(Node *head){
 	while(head!=NULL){
@@ -51,9 +43,7 @@ void print(Node *head){
    }
    cout<<endl;
 }
-
 //Recursive approaches :-----------------------------------------------------------------------------------------------
-
 //This is the 1st recursive approach to reverse LL----------------------------------------------------
 Node *reverseLinkedListRecSimple(Node *head)
 {
@@ -64,8 +54,6 @@ Node *reverseLinkedListRecSimple(Node *head)
 	//Calling Recursion On smaller Input
 	//storing the head of reversed LL in small
 	Node *small=reverseLinkedListRecSimple(head->next);
-	
-	
 	//Now traversing through the linked list using temp
 	//This traversing is to to attach the first element with 
 	//the obtained answer from the recursion 
@@ -74,16 +62,13 @@ Node *reverseLinkedListRecSimple(Node *head)
 	    temp=temp->next;
 	}
 	head->next =NULL;
-	temp->next = head;
-	  
+	temp->next = head; 
 	//returning the head of reversed linked list  
     return small;
 }
-
 //This is the 2nd recursive approach to reverse LL---------------------------------------------------------
 //This is a complicated One but will get idea of many concepts
 //This is a double node method
-
 //Pair Class is defined to maintain both the head and tha tail of the linkedlist
 //this will reduce the time complexity
 class Pair{
@@ -91,8 +76,7 @@ class Pair{
 		Node *head;
 		Node *tail;
 };
-
-//This a heper function to reverseLinkedListRecUsingPairClass Function
+//This a helper function to reverseLinkedListRecUsingPairClass Function
 //this returns the object of Pair class
 Pair reverseLinkedListRecUsingPairClassHelper(Node *head)
 {
@@ -102,34 +86,27 @@ Pair reverseLinkedListRecUsingPairClassHelper(Node *head)
     	ans.head = head;
     	ans.tail =head;
     	return ans;
-	}
-  	
+	}	
     //Calling Recursion On smaller Input
 	//storing the head of reversed LL in small
 	Pair small=reverseLinkedListRecUsingPairClassHelper(head->next);
-    
     //attach the first element with 
 	//the obtained answer from the recursion
 	small.tail->next =head;
 	head->next =NULL;
-	
 	//Making an object of Pair class to return that
 	Pair ans;
 	ans.head = small.head;
 	ans.tail = head;
 	return ans;
 }
-
 //Function that takes head of linkedList 
 //Gives that head to the helper function 
-
 Node* reverseLinkedListRecUsingPairClass(Node* head){
 	Pair a =reverseLinkedListRecUsingPairClassHelper(head);
-	
 	//returns only the head of the linked list
 	return a.head;
 }
-
 ////This is the 3rd recursive approach to reverse LL -----------------------------------------------
 //This easiest of above 2 approaches 
 Node *reverseLinkedListRecBest(Node *head)
@@ -137,42 +114,33 @@ Node *reverseLinkedListRecBest(Node *head)
 	//BaseCase for Recursion
     if(head ==NULL || head->next ==NULL){
     	return head;
-    }
-    
+    }  
     //Calling Recursion On smaller Input
 	//storing the head of reversed LL in small
-    Node *small=reverseLinkedListRecBest(head->next);
-  	
+    Node *small=reverseLinkedListRecBest(head->next);  	
 	//I above 2 methods there we find the need of finding 
 	//address of the end of the reversed linked list 
 	//but that address of the first element of the original ll
-	//is stored in the head .So no need to traverse it .
-	
+	//is stored in the head .So no need to traverse it .	
     Node*tail = head->next;
     tail->next = head;
-    head->next =NULL;
-  
+    head->next =NULL;  
     return small;
 }
-
 //Iterative approach :-----------------------------------------------------------------------------------------------
-
 //This is 4th approach to reverse linked lisr ---------------------------- 
 Node* reverseLinkedListIterative(Node* head)
 {
 	//current node stores the current while traversing
 	//prev node stores the previos node then current
-	//nexto node stores 1 node above the current node
-	
+	//nexto node stores 1 node above the current node	
 	Node* current = head;
 	Node* prev = NULL;
-	Node* nexto =current->next;
-	
+	Node* nexto =current->next;	
 	//traversing through the list and making the current node points
 	//to the previous node without loosing the address of any node
 	//this loop breaks when the current gets equal to NULL
-	//means that already the whole list is reversed
-	
+	//means that already the whole list is reversed	
 	while(current!=NULL){
 		current->next = prev;
 	    prev = current;
@@ -185,46 +153,36 @@ Node* reverseLinkedListIterative(Node* head)
 	//prev stores the address of first element of reversed list 
 	return prev;
 }
-
-
-
-int main(){
-	
+int main(){	
 	//taking the input from the user and storing it's head in head variable
+	//this take input function wille take inputs until user enters -1
 	cout<<"Enter the linked list: ";
 	Node *head =takeInput();
 	cout<<"Input linkedList is : ";
-	print (head);
-	
+	print (head);	
 	//1st approach
 	Node*a =reverseLinkedListRecSimple(head);
 	cout<<"Reversed LL by 1st approach :";
-	print (a);
-	
+	print (a);	
 	//2nd approach
 	Node *b =reverseLinkedListRecUsingPairClass(a);
 	cout<<"Reversed LL by 2nd approach :";
-	print(b);
-	
+	print(b);	
 	//3rd Approach
 	Node*c = reverseLinkedListRecBest(b);
 	cout<<"Reversed LL by 3rd approach :";
-	print(c);
-	
+	print(c);	
 	//4th approach
 	Node*d = reverseLinkedListIterative(c);
 	cout<<"Reversed LL by 4th approach :";
 	print(d);
-	
 	//above i reversed the same lnked list again and again
 	//i.e reversed the list reversed by first aprroach and so on
 	return 0;
 }
-
 //Approach 1----------
 	//time complexity: O(n^2) because recursion take O(n) and traversing the list again takes O(n) therefore int total O(n^2)
-	//Space Compleaxity:O(1)
-	
+	//Space Compleaxity:O(1)	
 //Approach 2----------
 	//time complexity: O(n) because recursion take O(n) and there is no need of traversing the list as in st approach
 	//Space Compleaxity:O(1)
@@ -234,5 +192,3 @@ int main(){
 //Approach 4----------
 	//time complexity: O(n) because we are iteration only once throught the whle list
 	//Space Compleaxity:O(1)
-	
-
