f3240cc2c8c666da03512efdfb348000542be528
Dhiraj Sharma <dhirajcoolssharma@gmail.com>
2018-06-24 14:19:25 +0530

    Update #489.cpp

--- Problem Solutions/Codeforces/Tags/math/#489.cpp
+++ Problem Solutions/Codeforces/Tags/math/#489.cpp
@@ -1,18 +1,16 @@
-#include<iostream>
-#include<cmath>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
-{
-    int x,y;
-    cin>>x;
-    cin>>y;
-    if(pow(x,y)>pow(y,x))
-        cout<< ">"<<endl ;
-    else if(pow(x,y)<pow(y,x))
-        cout<< "<"<<endl ;
+{   int x,y;
+    cin>>x>>y;
+    //Greater than 9*(10^-9) is used due to the condition on input x,y.
+    if (log10(x)*y-log10(y)*x>(9*1e-9))
+        cout<<">";
+    else if (-log10(x)*y+log10(y)*x>(9*1e-9))
+        cout<<"<";
     else
-        cout<< "="<<endl ;
-
+        cout<<"=";
     return 0;
-
 }
+//Used log10 because input is less than or equal to 1 for x,
+//But log1=0 hence to make the code work it is used.
