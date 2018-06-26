d20b5656d34a6fb0b7cc2b1f203e2b21cb0f662c
Anuj goyal <anujg935@gmail.com>
2018-06-26 18:33:30 +0530

    Made changes as said by mentor
    I made a helper function for gcd Euclid's algo which will call the gcd()function accordingly.so no need to find the minimum of 2 inputs.
    Fixed some commenting issues.
    Made explanation of gcdNaive() function more elobarated.

--- Algorithms/NumberTheory/GreatestCommonDivisor.cpp
+++ Algorithms/NumberTheory/GreatestCommonDivisor.cpp
@@ -6,21 +6,28 @@
 using namespace std;
 
 //Naive algorithm for finding GCD
-//this loop starts from 2 to a+b
+//this loop starts from 2 to the minimum of a and b
 //where a & b are 2 numbers
 int gcdNaive(int a, int b) {
-  int current_gcd = 1;
-  for (int d = 2; d <= a && d <= b; d++) {
-    if (a % d == 0 && b % d == 0) {
-      if (d > current_gcd) {
-        current_gcd = d;
-      }
-    }
+	
+	//in this algorithm we initialized the gcd with 1
+	//and run the loop from d=2 to the minimum of a and b
+	//in running the loop if for any value of d is factor of both a nad b
+	//the tha value is greater than the prev value of gcd then it will be updated.
+	int current_gcd = 1;
+	for (int d = 2; d <= a && d <= b; d++) {
+		if (a % d == 0 && b % d == 0) {
+    		if (d > current_gcd) {
+        		current_gcd = d;
+      		}
+    	}
   }
   return current_gcd;
 }
 
 //Euclid's algorithm for finding GCD
+
+//this algo is simmilar to that we studied in shools for finding hcf
 int gcd(int a, int b)
 {
 	int r=a%b;
@@ -36,6 +43,18 @@ int gcd(int a, int b)
 	
 }
 
+//this is a helper function to gcd() function because our gcd function 
+// is very precise about first and second arguements of function 
+//in gcd() function first arguement should be greater so we made a helper function for this purpose
+int gcdHelper(int a,int b){
+	if(a>=b){
+		return gcd(a,b);
+	}
+	else{
+		return gcd(b,a);
+	}
+}
+
 int main() {
   int a, b;
   //Get's the input from the user
@@ -43,21 +62,16 @@ int main() {
   cin >> a >> b;
   
   //Our Euclid's algorithm is precise about first and 2nd arguements
-  if(a>b){
-  	cout<<"GCD by Euclid's Algorithm is :"<<gcd(a,b)<<endl;
-  }
-  else{
-  	cout<<"GCD by Euclid's Algorithm is :"<<gcd(b,a)<<endl;;
-  }
+  cout << "GCD by Euclid's algorithm is :" << gcdHelper(a,b) << endl;
   
-  cout<<"GCD by naive algorithm is :"<<gcd(a, b)<<endl;
+  cout << "GCD by naive algorithm is :" << gcd(a, b) << endl;
   
   return 0;
 }
 
 //Time and Space Complexities--------
 //gcdNaive() function:- 
-	//Time Complexity: O(n) actually it is O(a+b) for two numbers a and b as loops runs a+b times which is covered in O(n).
+	//Time Complexity: O(n) because the loop runs for the minimum of the two inputs.
 	//Space Complexity: O(1)
 	
 //gcd() function:-
