f07e7cacdfa7117bfd8bce46edae8308263238ca
Ashish Agarwal <ashishagarwal2223@gmail.com>
2018-06-26 19:22:52 +0530

    Update Balanced_Parenthesis.cpp

--- Algorithms/Balanced_Parenthesis.cpp
+++ Algorithms/Balanced_Parenthesis.cpp
@@ -19,29 +19,26 @@ bool checkCharacter(char x, char y)
 bool checkBalancedParenthesis(string s) // Function to check if the string has balanced paranthesis or not
 {
 	stack<char> st; // character stack to store given string characters
-	int top=-1;     // initialize top as -1 as the stack is 0 indexed
 	int l=s.length();
 	for(int i=0;i<l;i++)
 	{
 		if(s[i]=='(' || s[i]=='{' || s[i]=='[') // if the character of the string is opening paranthesis, push it to stack
 		{
 		        st.push(s[i]);
-		        top=top+1;   // increament top by 1 as character is pushed
 		}
 		else if(s[i]==')' || s[i]=='}' || s[i]==']')  // if character is closing parenthesis, compare with stack top
 		{
-			if(top==-1 || checkCharacter(st.top(),s[i])==false) // if no element in stack or character at top does not match with corresponding current character
+			if(st.empty() || checkCharacter(st.top(),s[i])==false) // if no element in stack or character at top does not match with corresponding current character
 			{
 			      return false;
 			}
 			else
 			{
 			      st.pop(); // if matches , pop the top of stack 
-			      top=top-1; // decreament top by -1
 			}
 		}
 	}
-	if(top==-1) // if top is -1 it indicates that number of opening parenthesis is same as closing parenthesis
+	if(st.empty()) // if top is -1 it indicates that number of opening parenthesis is same as closing parenthesis
 	return true;
 	else
 	return false;
