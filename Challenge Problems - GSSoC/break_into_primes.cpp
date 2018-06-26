e2c40a4fc9046605db2269cd01d8b3a39116ae23
Sahil <shlbnsl843@gmail.com>
2018-06-26 16:04:15 +0530

    Add challenge problems by @ramitsawhney27

---
+++ Challenge Problems - GSSoC/break_into_primes.cpp
@@ -0,0 +1,12 @@
+/*
+Given:
+  A positive integer n > 1.
+
+Tasks:
+  Print a list of primes such that their sum is equal to n, maximize the number of primes in this list.
+  Add proper explanations and time complexity.
+
+Samples:
+  2 - 2
+  4 - 2+2
+*/
