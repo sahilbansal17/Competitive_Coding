c922c4f52d69e9cfac6aff2c569d003034250a5c
Ashish Agarwal <ashishagarwal2223@gmail.com>
2018-06-26 15:10:27 +0530

    Update Balanced_Parenthesis.cpp

--- Algorithms/Balanced_Parenthesis.cpp
+++ Algorithms/Balanced_Parenthesis.cpp
@@ -2,9 +2,11 @@
 #include <stack>
 #include <string>
 using namespace std;
-bool check(char x, char y)
+
+// Function to check if character x is an opening parenthesis and character y is an closing parenthesis or character x is 'less than' and character y is 'greater than' operator
+bool checkCharacter(char x, char y) 
 {
-	if ((x=='(' && y==')') || (x=='{' && y=='}') || (x=='[' && y==']') || (x=='<' && y=='>'))
+	if ((x=='(' && y==')') || (x=='{' && y=='}') || (x=='[' && y==']') || (x=='<' && y=='>')) // compare characters
 	{
 		return true;
 	}
@@ -14,46 +16,47 @@ bool check(char x, char y)
 	}
 }
 
-bool checkbalancedparenthesis(string s)
+bool checkBalancedParenthesis(string s) // Function to check if the string has balanced paranthesis or not
 {
-	stack<char> st;
-	int top=-1;
+	stack<char> st; // character stack to store given string characters
+	int top=-1;     // initialize top as -1 as the stack is 0 indexed
 	int l=s.length();
 	for(int i=0;i<l;i++)
 	{
-		if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+		if(s[i]=='(' || s[i]=='{' || s[i]=='[') // if the character of the string is opening paranthesis, push it to stack
 		{
 		st.push(s[i]);
-		top=top+1;
+		top=top+1;   // increament top by 1 as character is pushed
 		}
-		else if(s[i]==')' || s[i]=='}' || s[i]==']')
+		else if(s[i]==')' || s[i]=='}' || s[i]==']')  // if character is closing parenthesis, compare with stack top
 		{
-			if(top==-1 || check(st.top(),s[i])==false)
+			if(top==-1 || checkCharacter(st.top(),s[i])==false) // if no element in stack or character at top does not match with corresponding current character
 			{
 				return false;
 			}
 			else
 			{
-			st.pop();
-			top=top-1;
+			st.pop(); // if matches , pop the top of stack 
+			top=top-1; // decreament top by -1
 			}
 		}
 	}
-	if(top==-1)
+	if(top==-1) // if top is -1 it indicates that number of opening parenthesis is same as closing parenthesis
 	return true;
 	else
 	return false;
 }
 int main() 
 {
-	// your code goes here
 	int test;
+	cout<<"Enter the number of strings you want to check"<<endl;
 	cin>>test;
 	while(test-->0)
 	{
 	string s;
+        cout<<"Enter the string you want to check"<<endl;
 	cin>>s;
-	if(checkbalancedparenthesis(s)==true)
+	if(checkBalancedParenthesis(s)==true) // check if input stribg has balanced parenthesis or not
 	cout<<"BALANCED"<<endl;
 	else
 	cout<<"NOT BALANCED"<<endl;
