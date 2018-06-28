9855c6d32291a377d37252eec6149c1eb4347aad
monsij <biswalmonsij@gmail.com>
2018-06-28 17:13:42 +0530

    Reduce space complexity

--- Algorithms/Dynamic-Programming/Longest Increasing Subsequence.cpp
+++ Algorithms/Dynamic-Programming/Longest Increasing Subsequence.cpp
@@ -4,56 +4,56 @@
 //                    in increasing order.
 // Input Format : The first line contains n,the number of elements of the array.
 //                The second line contains n space separated integers.
-// Output Format : A single number specifying the length of LIS, followed by each element of LIS.
+// Output Format : A single number specifying the length of LIS.
 
 // Approach Used : Dynamic Programming
 
 // Intuition behind dynamic programming:
-// In this solution each element of the aux vector ,say at index i stores the LIS possible with the 
-// last element arr[i](ith element of the input array). So clearly, the first element of aux stores just arr[0].
+// In this solution each element of the aux vector ,say at index i stores the length of LIS possible with the 
+// last element arr[i](ith element of the input array). So clearly, the first element of aux stores just 1(only arr[0]).
 // Next for finding LIS for other elements as last element of that sequence, an iteration is made for
 // all the previous LIS whose last element is less than the current element. If such a sequence is found
-// the largest of these is taken with the last element appended at the end of it. Finally the sixe of the maximum sequence
-// along with the sequence is printed. The memorisation of previous LIS is a key to dynamic programming here and forms the
-// overlapping subproblems.
+// the largest of these is taken with the last element appended at the end of it(one is added actually to realise it). Finally the 
+// size of the maximum sequence is printed. The memoization of previous LIS length is a key to dynamic programming here 
+// and is the technique to store the solutions of subproblems.
  
 #include<iostream>
 #include<vector>
 using namespace std;
 
 
-void calLIS(vector<int>arr)
+void calLIS(vector<int>arr,int n)
 {
-	vector< vector<int> > aux(arr.size());
-	int final_ans=INT_MIN;
-	int position;
-	aux[0].push_back(arr[0]);
+	int aux[n];
 	
-	for(int i=1; i<arr.size(); i++)
+	// By default the sequence ending with first element can only be of length 1
+	aux[0]=1;
+	int final_ans = aux[0];
+	for(int i=1; i<n; i++)
 	{
-		// Iterating over all previous LIS
+		int curr_max=0;
+		
 		for(int j=0; j<i; j++)
 		{
-			
-			if( (arr[j] < arr[i]) && ( aux[i].size() < (aux[j].size())) )
+			// if last element is greater as well as its length is greater than the seen maximum
+			if(arr[j] < arr[i] && aux[j]> curr_max)
 			{
-				aux[i]=aux[j];
+				curr_max = aux[j];
 			}
 		}
-		// Appending current element at the end of it.
-		aux[i].push_back(arr[i]);
-		if(int(aux[i].size()) > final_ans)
+		
+		// 1 added to account the addition of the current element in the LIS.
+		aux[i] = curr_max + 1;
+		
+		// Simple comparison to store the maximum among all LIS length.
+		if(aux[i]>final_ans)
 		{
-			final_ans = int(aux[i].size());
-			position = i;
+			final_ans = aux[i];
 		}
 	}
-	
+		
 	cout << "Maximum size :" << final_ans << "\n";
-	cout << "Subsequence is: ";
-	for(int j=0; j<final_ans; j++)
-		cout << aux[position][j] << " ";
-	cout << "\n";
+
 }
 
 int main()
@@ -68,12 +68,12 @@ int main()
 		cin >> x;
 		arr.push_back(x);
 	}
-	calLIS(arr);
+	calLIS(arr,n);
 	return 0;
 }
 
 // Time complexity analysis: O(n^2)
 // As this approach may lead to a maximum of n-1 iterations for the nth element, the time complexity is of the order O(n^2).
 
-// Space complexity analysis: O(n^2)
-// Due to the use of vector aux of size n*n, the space complexity is O(n^2).
+// Space complexity analysis: O(n)
+// Due to the use of linear aux array of size n, the space complexity is O(n).
