f44f62bfe297f720b028e7f5a7e55709164e13cb
Jyoti Mishra <37954338+jstjyoti@users.noreply.github.com>
2018-06-28 22:21:32 +0530

    update heap.c

--- Algorithms/Divide-and-Conquer/heap.c
+++ Algorithms/Divide-and-Conquer/heap.c
@@ -11,13 +11,18 @@ Max Heap: A heap is called max heap if all the nodes having a value greater than
 This is used for Sorting in Ascending Order. 
 Min Heap: a heap is called min heap if all the nodes having smaller value than every children of the node '''
 */
+/*This function(swap()) is for swapping two values having Time Complexity O(1) and space Complexity O(1) as only one extra pointer required to swap values*/
 void swap(int *x,int *y)
 {
 	int t=*x;
 	*x=*y;
 	*y=t;
 }
-/*This function is for swapping two values having Time Complexity O(1) and space Complexity O(1) as only one estra pointer required to swap values*/
+/*This function(heap()) is responsible for creation a max heap everytime when it is called .
+Space Complexity of this is O(1) as only a few single variables are needed and they together represent a constant number of extra variables
+Time Complexity of above funtion is O(Logn) as the task is divided into two equal halves T(n) depends on Height as we are taking one of left and right subtrees to adjust the tree structure
+Height of balanced tree having n nodes=Logn
+*/
 void heap(int *arr, int n, int i)
 {
     int largest = i; 
@@ -33,29 +38,28 @@ void heap(int *arr, int n, int i)
         heap(arr, n, largest);
     }
 }
-/*This(above) function is responsible for creation a max heap everytime when it is called .
-Space Complexity of this is O(1) as only a few single variables are needed and they together represent a constant number of extra variables
-Time Complexity of above funtion is O(Logn) as the task is divided into two equal halves T(n)=T(Height as we are taking one of lest and right subtrees to adjust the tree structure)
-height of tree having n nodes=Logn
+/*This Function(heapSort()) is for Calling heap() functtion and causing sort, each time by removing last element after first and last elements are swapped.
+Space complexity of this is O(1) as each time only some constant number of Variables are needed.
+Time Coomplexity is O(nLogn) as it is calling heap() function(discussed complexity above the function) from an i loop having iteration n times(below i loop)
+first i loop has iterartions in order of n times so it is having complexity about O(nLogn)
+Therefore, Time Complexity is O(nLogn)
 */
 void heapSort(int *arr, int n)
 {
     int i;
+/*this loop is making max-heap because it is starting from last 2nd level(level just above leaf nodes)as parent and arranging it into
+heap and repeating the same upto the root node i.e.,heap function makes the root element having greatest value so,each parent having 
+higher values than their child nodes and this is condition of Max-Heap*/
     for (i = n/2 - 1; i >= 0; i--)
-        heap(arr, n, i);
+	    heap(arr, n, i);
     for (i=n-1; i>=0; i--)
     {
-      
         swap((arr+0), (arr+i));
- 		heap(arr, i, 0);
+ 		heap(arr, i, 0);//arranging remaining elements in heap O(logn)
+	   //loop having Complexity O(n log n)
     }
 }
-/*This Function is Fuction for Calling Heap method and causing sort by each time removing last element after first and last elements are swapped.
-Space complexity of this is O(1) as each time only some constant number of Variables are needed.
-Time Coomplexity is O(nLogn) as it is calling heap function from an i loop having iteration n times(below i loop)
-above i loop has iterartions lesser than n times so it is having complexity<O(nLogn)
-Therefore, Time Complexity is O(nLogn)
-*/
+ /*This Function is only for Displaying array*/
 void display(int *arr, int n)
 {
 	int i;
@@ -63,7 +67,7 @@ void display(int *arr, int n)
         printf("%d\t",*(arr+i));
     printf("\n");
 }
- /*This Function is only for Displaying array*/
+/*in Main we take input number of elements and unsorted array and call function to sort the array and display*/
 int main()
 {
     int *arr,i,n;
@@ -78,4 +82,3 @@ int main()
     display(arr, n);
 	return 0;
 }
-/*in Main we take input number of elements and unsorted array and call function to sort the array and display*/
