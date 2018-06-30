940238fcf7ad00956b889fe9ae4450a3c21e043c
Lavish Saluja <lavishsaluja.ls@gmail.com>
2018-06-30 23:28:18 +0530

    Added Increasing order in Priority Queue!

--- Data Structures/Queues/stl_priority_queue.cpp
+++ Data Structures/Queues/stl_priority_queue.cpp
@@ -1,10 +1,12 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<iostream>
+#include<queue>
+#include<vector>
 
-void ReversePriorityQueue(priority_queue<int> );
+using namespace std;
 
 int main(void){
 
+	cout<<"Decreasing Order in Priority Queue (default case)"<<endl;
 	// Declaring a priority queue which stores integers.
 	priority_queue<int> q;
 	/* By default the elements in priority queues are sorted in 
@@ -40,9 +42,37 @@ int main(void){
 	q.push(17);
 	q.push(7);
 
-	cout<<"The size of our queue now is"<<endl;
+	cout<<"Current size of our Queue is"<<endl;
 	cout<<q.size()<<endl;
 
 	cout<<endl;
+	// Similarly Let's demonstarte for Priority Queue in increasing order
+	// Note as said above the default is decreasing order but you can change it
+	// by changing the declaration of our priority_queue
+
+	cout<<"Increasing Order in Priority Queue"<<endl;
+
+	priority_queue<int, vector<int>, greater<int> > pq;
+
+	// All the functions and time complexities are same.
+	//Let's add some elements!
+	pq.push(2);
+	pq.push(1);
+	pq.push(4);
+
+	// Let's print the topmost value (on the head)
+	cout<<pq.top()<<endl;
+
+	// pop function removes the element
+	pq.pop();
+	cout<<pq.top()<<endl;
+	pq.pop();
+	cout<<pq.top()<<endl;
+	pq.pop();
+	pq.push(6);
+
+	cout<<"Current size of our queue is\n";
+	cout<<pq.size()<<endl;
+
 	return 0;
 }
