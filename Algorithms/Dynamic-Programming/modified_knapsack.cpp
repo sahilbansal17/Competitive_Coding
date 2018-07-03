58695fa69324dca9c9fef9434a7a340a31f6b1d2
Shivam Singhal <shivamsinghal0610@gmail.com>
2018-07-03 13:56:27 +0530

    Added modified knapsack algorithm

---
+++ Algorithms/Dynamic-Programming/modified_knapsack.cpp
@@ -0,0 +1,135 @@
+// Name : Shivam Singhal
+// Email ID : shivamsinghal0610@gmail.com
+// Modified Knapsack algorithm to obtain maximum weight from an array that can be put 
+// in a knapsack of given capacity with restricted number of weights to be taken
+
+#include<iostream>
+using namespace std;
+ 
+struct node{
+	int maxval;
+	int maxitems;
+};
+ 
+// Prints the maximum value that can be put in a knapsack of capacity W
+// such that the number of items taken is <= mit
+void knapSack(int W, int wt[], int val[], int n, int mit)
+{
+	int i, w;
+	node K[n+1][W+1];
+	for (i = 0; i <= n; i++)
+	{
+		for (w = 0; w <= W; w++)
+		{
+			// initialising with 0 if number of items or weight in consideration is 0
+			if (i==0 || w==0){
+				K[i][w].maxval = 0;
+				K[i][w].maxitems = 0;
+			}
+			// if weight of current item is <= knapsack capacity in consideration
+			else if (wt[i-1] <= w){
+				// deciding it current item is to be taken or not
+           				if((val[i-1] + K[i-1][w-wt[i-1]].maxval)>K[i-1][w].maxval){
+           					// checking if max items limit is exceeded or not
+           					if((1+K[i-1][w-wt[i-1]].maxitems)<=mit){
+           						K[i][w].maxval = val[i-1] + K[i-1][w-wt[i-1]].maxval;
+           						K[i][w].maxitems = 1+K[i-1][w-wt[i-1]].maxitems;
+           					}
+           					else{
+           						// selecting current item if it has max value
+           						if(val[i-1]>=K[i-1][w].maxval && val[i-1]>=K[i-1][w-wt[i-1]].maxval){
+           							K[i][w].maxval=val[i-1];
+           							K[i][w].maxitems=1;
+           						}
+           						else{
+           							if(K[i-1][w].maxval>K[i-1][w-wt[i-1]].maxval){
+           								K[i][w].maxval=K[i-1][w].maxval;
+           								K[i][w].maxitems = K[i-1][w].maxitems;
+           							}
+           							else if(K[i-1][w].maxval<K[i-1][w-wt[i-1]].maxval){
+           								K[i][w].maxval=K[i-1][w-wt[i-1]].maxval;
+           								K[i][w].maxitems = K[i-1][w-wt[i-1]].maxitems;
+           							}
+           							else{
+           								//selecting item which has minimum items taken already
+           								if(K[i-1][w].maxitems<=K[i-1][w-wt[i-1]].maxitems){
+           									K[i][w].maxval=K[i-1][w].maxval;
+           									K[i][w].maxitems = K[i-1][w].maxitems;
+           								}
+           								else{
+           									K[i][w].maxval=K[i-1][w-wt[i-1]].maxval;
+           									K[i][w].maxitems = K[i-1][w-wt[i-1]].maxitems;
+           								}
+           							}
+           						}
+           					}
+           				}
+           				//not taking current item in knapsack
+           				else if((val[i-1] + K[i-1][w-wt[i-1]].maxval) < K[i-1][w].maxval){
+            				K[i][w].maxval = K[i-1][w].maxval;
+            				K[i][w].maxitems = K[i-1][w].maxitems;
+           				}
+           				else{
+           					if(K[i-1][w-wt[i-1]].maxitems<=K[i-1][w].maxitems){ 
+           						if((1+K[i-1][w-wt[i-1]].maxitems)<=mit){
+           							K[i][w].maxval = val[i-1] + K[i-1][w-wt[i-1]].maxval;
+           							K[i][w].maxitems = 1+K[i-1][w-wt[i-1]].maxitems;
+           						}
+           						else{
+           							K[i][w].maxval = K[i-1][w].maxval;
+           							K[i][w].maxitems = K[i-1][w].maxitems;	
+           						}
+           					}
+           					else{
+           						K[i][w].maxval = K[i-1][w].maxval;
+           						K[i][w].maxitems = K[i-1][w].maxitems;
+           					}
+           				}
+			}
+			// if weight of current item exceeds knapsack weight in consideration, we simply take
+			// the best case for 1 the items that have been checked already for the same knapsack capacity
+			else{
+            			K[i][w].maxval = K[i-1][w].maxval;
+            			K[i][w].maxitems = K[i-1][w].maxitems;
+			}
+		}
+	}
+	cout<< "Maximum weight possible : " << K[n][W].maxval << endl;
+	cout<< "Number of items chosen : " << K[n][W].maxitems;
+}
+ 
+int main()
+{
+	int n, i, W, mit;
+	cin >> n;
+	cout << "Total number of elements : " << n << endl;
+	int wt[n], val[n];
+	for(i=0; i<n ; i++){
+		cin >> wt[i] >> val[i] ;
+	}
+	cout << "Weights of each item : ";
+	for(i=0; i<n ;i++)
+		cout << wt[i] << " ";
+	cout << endl;
+	cout << "Values of corresponding item : ";
+	for(i=0; i<n ;i++)
+		cout << val[i] << " ";
+	cout << endl;
+	cin >> W >> mit ;
+	cout << "Knapsack Capacity : " << W << endl;
+	cout << "Number of items permissible : " << mit << endl;
+	//function to calculate the maximum value obtainable taking items of total weight
+	// <= W and number of items <= mit
+	knapSack(W, wt, val, n, mit);
+	return 0;
+}
+
+// Time Complexity : O(nW)
+
+//Our modified algorithm is nothing but a dp-based approach which stores tha maximum number of items possible
+//such that kanpsack weight is not exceeded. It implements memoisation so we simply access previous values in
+//O(1) time. Also, our algorithm is entirely composed of if-else statements, each of which take O(1) time. The 
+//time complexity is therefore totally dependent on the 2 loops of n and W runtime each.
+
+// Auxiliary Space Complexity : O(nW)
+// This is because we consider taking a 2D array of size (n+1)*(W+1) for memoisation purposes. 
