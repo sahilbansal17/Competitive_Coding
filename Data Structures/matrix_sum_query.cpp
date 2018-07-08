0e29b51b4850cdf7884c949944ad6e9b42cdb772
Shivam Singhal <shivamsinghal0610@gmail.com>
2018-07-08 17:25:20 +0530

    Made changes requested by @sahilbansal17

--- Data Structures/matrix_sum_query.cpp
+++ Data Structures/matrix_sum_query.cpp
@@ -23,18 +23,20 @@ int main()
 			cout << a[i][j] << " ";
 		cout << endl;
 	}
+	// taking column wise sum
 	for(int i=1; i<m; i++){
 		for(int j=0; j<n; j++){
 			a[i][j] += a[i-1][j];
 		}
 	}
-	for(int i=1; i<m; i++){
+	// taking row wise sum 
+	for(int i=0; i<m; i++){
 		for(int j=1; j<n; j++){
 			a[i][j] += a[i][j-1];
 		}
 	}
-	for(int i=1 ; i<n ; i++)
-		a[0][i] += a[0][i-1];
+	// the sum matrix contains the sum contained in all elements
+	// from (0,0) to (i,j) at (i,j)
 	cout<<"Sum matrix:\n";
 	for(int i=0;i<m;i++){
 		for(int j=0;j<n;j++)
@@ -42,19 +44,31 @@ int main()
 		cout<<endl;
 	}
 	cin >> q;
+	cout << "Please enter the 2 co-ordinates for which sum is to be found.\n";
+	cout << "Make sure that the co-ordinates are 0-indexed.\n";
+	cout << "While the first co-ordinate should correspond to the top left co-ordinate, the second co-ordinate should correspond to the bottom right co-ordinate." << endl;
 	cout << "Number of queries : " << q << endl;
 	for(int i=1; i<=q; i++){
 		int ai, aj, bi, bj;
 		cin >> ai >> aj >> bi >> bj;
 		cout << "Query #" << i << " : " << "(" << ai << "," << aj << ") to (" << bi << "," << bj << ")" << endl;
+		// in case of improper rectangle
 		if	(ai > bi || aj > bj)
 			cout << "Invalid Query !" << endl;
 		else{
+			// sum of all elements from (0,0) to (bi,bj), i.e., the larger rectangle
 			int value = a[bi][bj];
+			// it can be seen that we need to remove the value of some elements from the total sum 
+			// obtained so far. We first remove the sub-matrix which occurs to the left of the required
+			// sub-matrix, i.e. (0,0) to (bi, aj-1)
 			if (aj > 0)
 				value -= a[bi][aj-1];
+			// we now remove the sub-matrix which occurs on top of the required sub-matrix, i.e., (0,0) to
+			// (a1-1,bj)
 			if(ai > 0)
 				value -= a[ai-1][bj];
+			// If observed carefully, it can be seen that the elements in sub-matrix (0,0) to (ai-1,aj-1) have 
+			// been subtracted twice, so we add that again
 			if(ai > 0 && aj > 0)
 				value += a[ai-1][aj-1];
 			cout << "Output of given query is : " << value << endl;
@@ -64,5 +78,7 @@ int main()
 }
 
 // Time Complexity : O(mn)
+// We are using a nested loop of m*n which helps us pre-calculate the sum matrix. Now, each query can 
+// easily be executed in O(1) time using mathematics and intuition.
 
 // Auxiliary Space Complexity : O(1)
