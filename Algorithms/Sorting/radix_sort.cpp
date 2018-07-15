4e47700020be5c4d490572b54a0289ba90d91457
Harsh <harsh.26020@gmail.com>
2018-07-15 11:28:47 +0530

    Remove extra spaces

--- Algorithms/Sorting/radix_sort.cpp
+++ Algorithms/Sorting/radix_sort.cpp
@@ -1,10 +1,14 @@
+// Radix sort is quite similar to sorting strings lexicographically. Out of any two string, one having larger
+// character at the first point of difference would be lexicographically greater.
+// We can extend this to integers if we pad up all integers with 0s to its left such that each has the same 
+// number of digits.
+// So what we do in radix sort is we start from the LSD and sort each number on the basis of the digit at current
+// place using counting sort. We move from LSD to MSD and eventually the array is sorted.
 #include <iostream>
 #include <vector>
 using namespace std;
-
 const int L = 1e5 + 7;
 int arr[L];
-
 void countingSort(int place, int number_of_elements)
 {
 	std::vector<int> buckets[10];
@@ -16,7 +20,6 @@ void countingSort(int place, int number_of_elements)
 		buckets[digit].push_back(arr[i]);
 	}
 	int idx = 0;
-	
 	// Starting from the first bucket elements are placed into the base Array as the elements
 	// in bucket having smaller index would be smaller(considering only digits upto the current place)
 	// Also elements in the same bucket will be in increasing order on the basis of digit at place-1
@@ -53,29 +56,19 @@ int main()
 	cout << "Enter number of elements = ";
 	cin >> number_of_elements;
 	cout << "Enter elements ";
-	
 	for (int i = 0; i < number_of_elements; ++i)
 	{
 		cin >> arr[i];
 		// finding the maximum element in the input array
 		max_element = max(max_element, arr[i]);
 	}
-
 	radixSort(number_of_elements, max_element);
-	
 	for (int i = 0; i < number_of_elements; ++i)
 	{
 		cout << arr[i] << " ";
 	}
 	return 0;
 }
-// Radix sort is quite similar to sorting strings lexicographically. Out of any two string, one having larger
-// character at the first point of difference would be lexicographically greater.
-// We can extend this to integers if we pad up all integers with 0s to its left such that each has the same 
-// number of digits.
-// So what we do in radix sort is we start from the LSD and sort each number on the basis of the digit at current
-// place using counting sort. We move from LSD to MSD and eventually the array is sorted.
-
 // Time Complexity : O(nk) where k is the number of digits in the max_element
 //                   The loop inside radixSort function runs k = log10(max_element) times.In each iteration
 //                   of the loop countingSort function is called, whose complexity is O(n) as the loop inside countingSort 
