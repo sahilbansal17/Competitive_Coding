ed186ac08638f510ec2899a8bf9cc50bdb645e5c
Sandip Kumar <sk1957339@gmail.com>
2018-07-15 09:49:57 +0530

    Update

--- Algorithms/Backtracking/Sudoku.java
+++ Algorithms/Backtracking/Sudoku.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
+//Name:Sandip Kumar (sandip75)
+//E mail-sk1957339@gmail.com
+//Question - We are given an sudoku with some intail value and need to fill it completely.
 public class Sudoku {
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-    // Taking input ( if blank take is as Zero ) 
+   	// Taking input ( if blank take is as Zero ) 
 		int[][] arr = new int[9][9];
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
@@ -17,7 +19,7 @@ public class Sudoku {
 		int[] cols = new int[9];
 		int[][] submats = new int[3][3];
 
-    // Here i will set the bit who values( from 1 to 9 ) are used in row , col and submats in given input   
+   	// Here i will set the bit who values( from 1 to 9 ) are used in row , col and submats in given input   
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
 				rows[i] |= (1 << arr[i][j]);
@@ -29,9 +31,9 @@ public class Sudoku {
 	}
 
 	private static void SolveSudoku(int[][] arr, int cellNo, int[] rows, int[] cols, int[][] submats) {
-  // Base case
+ 	// Base case
 		if (cellNo > arr.length * arr[0].length) {
-    // Printing the solution 
+   	// Printing the solution 
 			for (int i = 0; i < rows.length; i++) {
 				for (int j = 0; j < cols.length; j++) {
 					System.out.print(arr[i][j] + " ");
@@ -41,17 +43,17 @@ public class Sudoku {
 			return;
 		}
 
-    // Coverting 2D array to 1D arrray 
+   	// Coverting 2D array to 1D arrray 
 		int row = (cellNo - 1) / arr.length;
 		int col = (cellNo - 1) % arr.length;
     
-    // When value is already present  at any [row][col]
+    	// When value is already present  at any [row][col]
 		if (arr[row][col] != 0) {
 			SolveSudoku(arr, cellNo + 1, rows, cols, submats);
 		} 
     
-    // When value is zero at any [row][col]
-    else {
+    	// When value is zero at any [row][col]
+    		else {
 			for (int choice = 1; choice <= 9; choice++) {
 				if (isNumberAvailable(arr, rows, cols, submats, choice, row, col)) {
 					reserveANumber(arr, rows, cols, submats, choice, row, col);
@@ -83,7 +85,7 @@ public class Sudoku {
 
 	private static void reserveANumber(int[][] arr, int[] rows, int[] cols, int[][] submats, int choice, int r, int c) {
 
-     // Here I will set that bit which I keep at given index ( row , col ) and will do | to keep track of pre.  bit also  
+     	// Here I will set that bit which I keep at given index ( row , col ) and will do | to keep track of pre.  bit also  
      
 		int mask = 1 << choice;
 		arr[r][c] = choice;
@@ -98,7 +100,7 @@ public class Sudoku {
     
     // Here I will free that number ( choice ). for that i will set choice bit as Zero and all 1. after that i will do & to get back all set bits.
     
-    int mask = ~(1 << choice);
+    		int mask = ~(1 << choice);
 		arr[r][c] = 0;
 
 		rows[r] &= mask;
@@ -106,3 +108,5 @@ public class Sudoku {
 		submats[r / 3][c / 3] &= mask;
 	}
 }
+// Time Complexity: O(m^(n^2)), where m is total number of empty cells. At every place there can be n possiblity. 
+// Space Complexity: O(9*3), Since we have used three array of size 9 ( rows[] , cols[] , submats[][] )
