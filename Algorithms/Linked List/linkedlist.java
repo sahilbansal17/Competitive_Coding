95c372b0b195b62563f73c31939d4368917fd62a
Aastha <anejaaastha@gmail.com>
2018-07-25 19:50:06 +0530

    added user prompts

--- Algorithms/Linked List/linkedlist.java
+++ Algorithms/Linked List/linkedlist.java
@@ -1,3 +1,4 @@
+
 //A linked list is said to contain a cycle if any node is visited more than once while 
 //traversing the list. The problem is to determine if a given Linked list contains a 
 //cycle or not. (Floyd’s Cycle-Finding Algorithm)
@@ -6,6 +7,9 @@
 //		Email - anejaaastha@gmail.com
 
 // This class implements a Linked List with some basic methods
+
+import java.util.Scanner;
+
 public class linkedlist {
 
 	// implementation of class Node (a node of the linked list)
@@ -148,25 +152,74 @@ public class linkedlist {
 		return false;
 	}
 
-	public static void main(String[] args) {
+	// This function returns the node at a particular index in the linked list
+	private Node getNodeAt(int index) throws Exception {
+
+		// If the index entered by user is invalid
+		if (index < 0 || index >= this.size) {
+			throw new Exception("Invalid index");
+		}
+
+		// We take a counter to keep track of the index of the node we are at
+		int counter = 0;
+
+		// and we start traversing the list from its head
+		Node temp = this.head;
+
+		// we traverse the list until we have reached the required node
+		while (counter < index) {
+
+			// this is done by moving temp pointer to the next node
+			temp = temp.next;
+
+			// and increasing the value of counter
+			counter++;
+		}
+
+		// finally, we return the required node
+		return temp;
+	}
+
+	// This function makes the next of the node at start point to the node at end
+	public void createCycle(int start, int end) throws Exception {
+		Node startNode = this.getNodeAt(start);
+		Node endNode = this.getNodeAt(end);
+		startNode.next = endNode;
+	}
+
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
 
 		// declaring a linked list
 		linkedlist list = new linkedlist();
 
+		System.out.println("Enter number of elements you want to add in the linked list:");
+		int n = in.nextInt();
+
 		// adding elements in the list
-		list.addFirst(10);
-		list.addLast(20);
-		list.addLast(30);
-		list.addLast(40);
-		list.addLast(50);
+		for (int i = 0; i < n; ++i) {
+			System.out.println("Enter element:");
+			list.addLast(in.nextInt());
+		}
+
+		System.out.println("Do you want to add a cycle in the linked list? (true/false)");
+		boolean flag = in.nextBoolean();
 
 		// creating a cycle
-		list.tail.next = list.head.next.next;
+		if (flag) {
+			System.out.println("Enter index of first node for cycle (0-based indexing) :");
+			int start = in.nextInt();
+			System.out.println("Enter index of second node for cycle (0-based indexing) :");
+			int end = in.nextInt();
+
+			list.createCycle(start, end);
+		}
 
 		// function call
 		if (list.hasCycle()) {
-			System.out.println("The list has a cycle");
+			System.out.println("The linked list has a cycle");
 		} else {
 			System.out.println("The list doesn't have a cycle");
 		}
