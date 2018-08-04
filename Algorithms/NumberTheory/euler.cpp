85eae654695856166a99c427589e72b67d1a2eab
Indraneel Ghosh <indraneel.ghosh1998@gmail.com>
2018-08-04 19:08:16 +0530

    Added necessary math background

--- Algorithms/NumberTheory/euler.cpp
+++ Algorithms/NumberTheory/euler.cpp
@@ -1,5 +1,8 @@
 /*In number theory, the totient φ of a positive integer n is defined to be the number of 
 positive integers less than or equal to n, that are coprime to n.
+For calculating Euler's totient function we use the following relation 
+φ(n) = n* (1-(1/p1))*(1-(1/p2)).....(1-(1/pn))
+where p1,p2,...,pn are prime numbers which can divide n
 */
 #include <bits/stdc++.h>
 using namespace std;
