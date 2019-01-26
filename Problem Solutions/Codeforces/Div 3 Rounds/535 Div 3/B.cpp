8eac9bd04fb456f8bcd9493493cc8e2aacb72e32
Sahil <shlbnsl843@gmail.com>
2019-01-26 22:54:18 +0530

    Add solutions to recently given contests

---
+++ Problem Solutions/Codeforces/Div 3 Rounds/535 Div 3/B.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Template file for Online Algorithmic Competitions */
+/* Typedefs */
+    /* Basic types */
+    typedef long long           ll;
+    typedef long double         ld;
+    typedef unsigned long long ull;
+    /* STL containers */
+    typedef vector <int>    vi;
+    typedef vector <ll>     vll;
+    typedef pair <int, int> pii;
+    typedef pair <ll, ll>   pll;
+    typedef vector < pii >  vpii;
+    typedef vector < pll >  vpll;
+    typedef vector <string> vs;
+    typedef vector < vi >   vvi;
+    typedef vector < vll >  vvll;
+    typedef vector < vpii > vvpii;
+    typedef set <int>       si;
+/* Macros */
+    /* Loops */
+    #define fl(i, a, b)     for(int i(a); i <= (b); i ++)
+    #define rep(i, n)       fl(i, 1, (n))
+    #define loop(i, n)      fl(i, 0, (n) - 1)
+    #define rfl(i, a, b)    for(int i(a); i >= (b); i --)
+    #define rrep(i, n)      rfl(i, (n), 1)
+    /* Algorithmic functions */
+    #define srt(v)          sort((v).begin(), (v).end())
+    /* STL container methods */
+    #define pb              push_back
+    #define mp              make_pair
+    #define eb              emplace_back
+    /* String methods */
+    #define dig(i)          (s[i] - '0')
+    #define slen(s)         s.length()
+    /* Shorthand notations */
+    #define fr              first
+    #define sc              second
+    #define re              return 
+    #define sz(x)           ((int) (x).size())
+    #define all(x)          (x).begin(), (x).end()
+    #define sqr(x)          ((x) * (x))
+    #define fill(x, y)      memset(x, y, sizeof(x))
+    #define clr(a)          fill(a, 0)
+    #define endl            '\n'
+    /* Mathematical */
+    #define IINF            0x3f3f3f3f
+    #define LLINF           1000111000111000111LL
+    #define PI              3.14159265358979323
+    /* Debugging purpose */
+    #define trace1(x)                cerr << #x << ": " << x << endl
+    #define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl
+    #define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl
+    /* Fast Input Output */
+    #define FAST_IO                  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+/* Constants */
+    const ll MOD = 1000000007LL;
+    const ll MAX = 100010LL;
+/* Templates */
+template<class T> T abs(T x) { re x > 0 ? x : -x; }
+template<typename T> T gcd(T a, T b){ if(b == 0) return a; return gcd(b, a % b); }
+template<typename T> T power(T x, T y, ll m = MOD){T ans = 1; x %= m; while(y > 0){ if(y & 1LL) ans = (ans * x)%m; y >>= 1LL; x = (x*x)%m; } return ans%m; }
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/Users/sahilbansal/Desktop/input.txt", "r", stdin);
+    freopen("/Users/sahilbansal/Desktop/output.txt", "w", stdout);
+    freopen("/Users/sahilbansal/Desktop/error.txt", "w", stderr);
+    #endif
+
+    FAST_IO;    
+
+    int n, factor;
+    cin >> n;
+
+    vi facs, done(10010, 0), vis(n + 1, 0);
+    rep (i, n) {
+        cin >> factor;
+        facs.pb(factor);
+    }
+
+    srt(facs);
+
+    int x = facs[n - 1];
+    done[x] = 1;
+    vis[n - 1] = 1;
+    loop (i, n) {
+        if (!done[facs[i]] && (x % facs[i] == 0)) {
+            done[facs[i]] = 1;
+            vis[i] = 1;
+        }
+    }
+
+    int y = 0;
+    rfl (i, n - 1, 0) {
+        if (!vis[i]) {
+            y = facs[i];
+            break;
+        }
+    }
+
+    cout << x << " " << y << endl;
+    return 0;
+}
---
+++ Problem Solutions/Codeforces/Div 3 Rounds/535 Div 3/C.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Template file for Online Algorithmic Competitions */
+/* Typedefs */
+    /* Basic types */
+    typedef long long           ll;
+    typedef long double         ld;
+    typedef unsigned long long ull;
+    /* STL containers */
+    typedef vector <int>    vi;
+    typedef vector <ll>     vll;
+    typedef pair <int, int> pii;
+    typedef pair <ll, ll>   pll;
+    typedef vector < pii >  vpii;
+    typedef vector < pll >  vpll;
+    typedef vector <string> vs;
+    typedef vector < vi >   vvi;
+    typedef vector < vll >  vvll;
+    typedef vector < vpii > vvpii;
+    typedef set <int>       si;
+/* Macros */
+    /* Loops */
+    #define fl(i, a, b)     for(int i(a); i <= (b); i ++)
+    #define rep(i, n)       fl(i, 1, (n))
+    #define loop(i, n)      fl(i, 0, (n) - 1)
+    #define rfl(i, a, b)    for(int i(a); i >= (b); i --)
+    #define rrep(i, n)      rfl(i, (n), 1)
+    /* Algorithmic functions */
+    #define srt(v)          sort((v).begin(), (v).end())
+    /* STL container methods */
+    #define pb              push_back
+    #define mp              make_pair
+    #define eb              emplace_back
+    /* String methods */
+    #define dig(i)          (s[i] - '0')
+    #define slen(s)         s.length()
+    /* Shorthand notations */
+    #define fr              first
+    #define sc              second
+    #define re              return 
+    #define sz(x)           ((int) (x).size())
+    #define all(x)          (x).begin(), (x).end()
+    #define sqr(x)          ((x) * (x))
+    #define fill(x, y)      memset(x, y, sizeof(x))
+    #define clr(a)          fill(a, 0)
+    #define endl            '\n'
+    /* Mathematical */
+    #define IINF            0x3f3f3f3f
+    #define LLINF           1000111000111000111LL
+    #define PI              3.14159265358979323
+    /* Debugging purpose */
+    #define trace1(x)                cerr << #x << ": " << x << endl
+    #define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl
+    #define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl
+    /* Fast Input Output */
+    #define FAST_IO                  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+/* Constants */
+    const ll MOD = 1000000007LL;
+    const ll MAX = 200010LL;
+/* Templates */
+template<class T> T abs(T x) { re x > 0 ? x : -x; }
+template<typename T> T gcd(T a, T b){ if(b == 0) return a; return gcd(b, a % b); }
+template<typename T> T power(T x, T y, ll m = MOD){T ans = 1; x %= m; while(y > 0){ if(y & 1LL) ans = (ans * x)%m; y >>= 1LL; x = (x*x)%m; } return ans%m; }
+
+int check(string s, string checker) {
+    int check_id = 0;
+    int s_id = 0;
+    int n = s.length();
+    int res = 0;
+    while (s_id < n) {
+        if (s[s_id] != checker[check_id]) {
+            res ++;
+        }
+        s_id ++;
+        check_id += 1;
+        check_id %= 3;
+    }
+    return res;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/Users/sahilbansal/Desktop/input.txt", "r", stdin);
+    freopen("/Users/sahilbansal/Desktop/output.txt", "w", stdout);
+    freopen("/Users/sahilbansal/Desktop/error.txt", "w", stderr);
+    #endif
+
+    FAST_IO;    
+
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    vector <string> checker;
+    checker.pb("RGB");
+    checker.pb("RBG");
+    checker.pb("BRG");
+    checker.pb("BGR");
+    checker.pb("GBR");
+    checker.pb("GRB");
+
+    int min_dist = MAX, cur_dist, res;
+    loop (i, 6) {
+        cur_dist = check(s, checker[i]);
+        if (cur_dist < min_dist) {
+            min_dist = cur_dist;
+            res = i;
+        }
+    }
+    
+    cout << min_dist << endl;
+    int res_id = 0, s_id = 0;
+    while (s_id < n) {
+        cout << checker[res][res_id];
+        s_id ++;
+        res_id += 1;
+        res_id %= 3;
+    }
+    return 0;
+}
