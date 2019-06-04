b198aa451c26c92b8845845c5cabc46ecbfc38c4
megha070 <varshney.megha070@gmail.com>
2019-06-04 20:53:52 +0530

    coin_change

--- Algorithms/Dynamic-Programming/coin_change.java
+++ Algorithms/Dynamic-Programming/coin_change.java
@@ -1,3 +1,4 @@
+//Program to find number of ways to reach a particular value by existing coins
 import java.util.*;
 import java.io.*;
 class coin_change
@@ -9,18 +10,23 @@ class coin_change
         while(testcase>0)
         {
             int value=s.nextInt();
+            //total value to be reached
             int n=s.nextInt();
+            //number of coins
             int[] coins=new int[n];
+            //array consisting of values of coins
             int i,j;
             for(i=0;i<n;i++)
             {
                 coins[i]=s.nextInt();
             }
             int[][] table=new int[coins.length+1][value+1];
+            //construct a 2D array to store the result
             for(i=0;i<=coins.length;i++)
             {
                 table[i][0]=1;
             }
+            //base case if value is 0
             for(i=0;i<=value;i++)
             {
                 table[0][i]=0;
@@ -40,3 +46,4 @@ class coin_change
         }
     }
 }
+//Time Complexity of the above code is O(mn) where m=value n=number of coins
