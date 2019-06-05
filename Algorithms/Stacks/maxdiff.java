446741d5031b16fba7c2e83fed4d38029aa743a9
megha070 <varshney.megha070@gmail.com>
2019-06-05 16:10:45 +0530

    maxdiff

--- Algorithms/Stacks/maxdiff.java
+++ Algorithms/Stacks/maxdiff.java
@@ -22,54 +22,54 @@ class maxdiff
             testcase--;
         }
     }
-     public static int getDiff(int a[], int n)
+    public static int getDiff(int a[], int n)
     {
-     ArrayList<Integer> left=getarray(a,n);
-     //Arraylist consisting of nearest left smaller element 
-     int[] b=reverse(a);
-     //b is an array which is reverse of original array
-     //a is reversed to get the nearest right smaller element of each element in an array
-     ArrayList<Integer> right=getarray(b,n);
-     int t=right.size();
-     int ans=Integer.MIN_VALUE;
-     for(int i=0;i<t;i++)
-     {
-         int ch=Math.abs(right.get(i)-left.get(t-1-i));
-         //get absolute difference between right and left smaller elements
-         ans=Math.max(ans,ch);
-     }
-     return ans;
+        ArrayList<Integer> left=getarray(a,n);
+        //Arraylist consisting of nearest left smaller element 
+        int[] b=reverse(a);
+        //b is an array which is reverse of original array
+        //a is reversed to get the nearest right smaller element of each element in an array
+        ArrayList<Integer> right=getarray(b,n);
+        int t=right.size();
+        int ans=Integer.MIN_VALUE;
+        for(int i=0;i<t;i++)
+        {
+            int ch=Math.abs(right.get(i)-left.get(t-1-i));
+            //get absolute difference between right and left smaller elements
+            ans=Math.max(ans,ch);
+        }
+        return ans;
     }
     //function to get an arraylist consisting of left smaller elements
-  public static ArrayList<Integer> getarray(int a[],int n)
+    public static ArrayList<Integer> getarray(int a[],int n)
   {
-      Stack<Integer> st=new Stack<>();
-     ArrayList<Integer> arr=new ArrayList<>();
-     int j=0;
-     for(int i=0;i<n;i++)
-     {
-         while(!st.isEmpty()&&st.peek()>=a[i])
-         st.pop();
+        Stack<Integer> st=new Stack<>();
+        ArrayList<Integer> arr=new ArrayList<>();
+        int j=0;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.isEmpty()&&st.peek()>=a[i])
+            st.pop();
          
-         if(st.isEmpty())
-         arr.add(0);
-         //for the element at extreme left the smallest left element is 0
-         else
-         arr.add(st.peek());
-         
-         st.push(a[i]);
-     }
-     return arr;
+            if(st.isEmpty())
+            arr.add(0);
+            //for the element at extreme left the smallest left element is 0
+            else
+            arr.add(st.peek());
+            
+            st.push(a[i]);
+        }
+        return arr;
   }
   //function to reverse the original array
   public static int[] reverse(int a[])
   {
-      int[] arr=new int[a.length];
-      int k=0;
-      for(int i=arr.length-1;i>=0;i--)
-      {
-          arr[k++]=a[i];
-      }
-      return arr;
+        int[] arr=new int[a.length];
+        int k=0;
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            arr[k++]=a[i];
+        }
+        return arr;
   }
 }
