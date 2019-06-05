87b3e5488cd622dd94f16b9bb9303c9cfc1aa3f8
megha070 <varshney.megha070@gmail.com>
2019-06-05 23:03:49 +0530

    nxtgrt

--- Algorithms/Stacks/next_greater.java
+++ Algorithms/Stacks/next_greater.java
@@ -7,7 +7,7 @@ class next_greater
  {
 	public static void main (String[] args)throws IOException
 	{
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
         int testcase = Integer.parseInt(br.readLine()); 
         while(testcase>0)
         {
@@ -17,12 +17,12 @@ class next_greater
             String[] strs = line.trim().split("\\s+"); 
             for (int i = 0; i < n; i++) 
                 arr[i] = Long.parseLong(strs[i]); 
-           Stack<Integer> st=new Stack<>();
+            Stack<Integer> st = new Stack<>();
 	    int i;
-	    long[] ans=new long[n];
+	    long[] ans = new long[n];
 	    for(i=0;i<n;i++)
 	    {
-	         ans[i]=-1;//initialise all the elements by -1
+	        ans[i] = -1;//initialise all the elements by -1
 	    }
 	    for(i=0;i<n;i++)
 	    {
@@ -30,9 +30,9 @@ class next_greater
             //if the element at the current position is greater than the position of element in stack
             //it is the next greatest element 
 	        {
-	            int t=st.peek();
+	            int t = st.peek();
 	            st.pop();
-	            ans[t]=arr[i];
+	            ans[t] = arr[i];
 	        }
 	        st.push(i);//push the index of current element
 	    }
@@ -40,8 +40,8 @@ class next_greater
 	    {
 	        System.out.print(ans[i]+" ");
 	    }
-            System.out.println();
-            testcase--;
+        System.out.println();
+        testcase--;
         }
 	}
 }
