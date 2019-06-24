5965023b9dea141722d42ab9e800eb6099acaec0
Naba7 <dashnabanita@gmail.com>
2019-06-24 20:52:05 +0530

    bubble_sort.py

--- Algorithms/Sorting/bubble_sort.py
+++ Algorithms/Sorting/bubble_sort.py
@@ -4,11 +4,11 @@
 # find the element in array by insertion sort
 # Code
 def bubble_sort(arr):
-    n = len(arr) 
-    for i in range(n): 
-        for j in range(0, n-i-1): 
-            if arr[j] > arr[j+1] : 
-                arr[j], arr[j+1] = arr[j+1], arr[j] 
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
 # Test-Case
 arr = [1, 13, 78, 6, 45689 78, 678, 45689]
@@ -22,4 +22,3 @@ for i in range(len(arr)):
 
 # Space-Complexity
 # O(1)
-
