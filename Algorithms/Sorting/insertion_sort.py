7da52fc99c74ae96a8c4b3518722c2ff1d3d4f02
Naba7 <dashnabanita@gmail.com>
2019-06-24 20:51:40 +0530

    added insertion sort

---
+++ Algorithms/Sorting/insertion_sort.py
@@ -0,0 +1,26 @@
+# Name : Nabanita Dash
+# Email ID : dashnabanita@gmail.com
+
+# find the element in array by insertion sort
+# Code
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+
+        k = arr[i]
+        j = i-1
+        while j >= 0 and k < arr[j] :
+                arr[j + 1] = arr[j]
+                j -= 1
+        arr[j + 1] = k
+
+# Test-Case
+arr = [1, 13, 78, 6, 45689 78, 678, 45689]
+insertion_sort(arr)
+for i in range(len(arr)):
+	print(arr[i],"\n")
+
+# Time-Complexity
+# Worst-Case : O(n*n)
+
+# Space-Complexity
+# O(1)
