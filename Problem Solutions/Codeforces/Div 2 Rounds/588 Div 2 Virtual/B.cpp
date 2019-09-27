3279310a2cf70688b14ac692e3b9ce5fe7a541b6
Sahil Bansal <shlbnsl843@gmail.com>
2019-09-28 01:05:40 +0530

    Add Evaluate Expression problem solution

---
+++ Problem Solutions/Codeforces/Div 2 Rounds/588 Div 2 Virtual/B.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem Link: https://codeforces.com/contest/1230/problem/B
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+typedef long long ll;
+
+int main() {
+  FAST_IO;
+  #ifndef ONLINE_JUDGE
+  freopen("/Users/sahilbansal/Desktop/input.txt", "r", stdin);
+  freopen("/Users/sahilbansal/Desktop/output.txt", "w", stdout);
+  freopen("/Users/sahilbansal/Desktop/error.txt", "w", stderr);
+  #endif 
+
+  
+
+  return 0;
+}
