6159dc31f9e5c83eab4a6e658d89bf6125f44a42
Sachin kumar <46827344+Sachindrck@users.noreply.github.com>
2019-10-01 20:31:31 +0530

    updated one

--- Data Structures/Stacks/postfix_to_infix.py
+++ Data Structures/Stacks/postfix_to_infix.py
@@ -1,34 +1,32 @@
-# python3 program to find infix for a given postfix. 
-def isOperand(x): 
-	return ((x >= 'a' and x <= 'z') or
-			(x >= 'A' and x <= 'Z')) 
+from __future__ import division
+import random
 
-#finding infix 
-def getInfix(exp) : 
 
-	s = [] 
+OPERATORS = set(['+', '-', '*', '/', '(', ')'])
+PRIORITY = {'+': 1, '-': 1, '*': 2, '/': 2}
 
-	for i in exp:	 
-		 
-		if (isOperand(i)) :		 
-			s.insert(0, i) 
-			
-		# we are assuming that input is a valid postfix and expect an operator. 
-		else: 
-		
-			op1 = s[0] 
-			s.pop(0) 
-			op2 = s[0] 
-			s.pop(0) 
-			s.insert(0, "(" + op2 + i +
-							op1 + ")") 
-			 
-	return s[0] 
 
-# Driver Code 
-if __name__ == '__main__': 
+def postfix_to_infix(formula):
+    stack = []
+    prev_op = None
+    for ch in formula:
+        if ch not in OPERATORS:
+            stack.append(ch)
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            if prev_op and len(a) > 1 and PRIORITY[ch] > PRIORITY[prev_op]:
+                # if previous operator has lower priority
+                # add '()' to the previous a
+                expr = '('+a+')' + ch + b
+            else:
+                expr = a + ch + b
+            stack.append(expr)
+            prev_op = ch
+    return stack[-1]
 
-	exp = "ab*c+"
-	print(getInfix(exp.strip())) 
+# driver code
+if __name__ == '__main__':
+    print(postfix_to_infix(input()))
 
- 
+# '1346*+61*+2*3/+'
