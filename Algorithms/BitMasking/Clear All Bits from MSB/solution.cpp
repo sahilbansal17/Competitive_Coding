1e54fbc5fff24e2670fd6fce3c97280a0e06c8bc
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2019-10-08 19:04:04 +0530

    Add stack implementation in python3 (#459)

---
+++ Algorithms/BitMasking/Clear All Bits from MSB/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int clearAllBits(int n, int i){
+    int mask = (1<<i) - 1;
+    return n&mask;
+}
+int main() {
+	int n, i;
+	cin >> n >> i;
+	cout<< clearAllBits(n, i) <<endl;
+	return 0;
+}
+
---
+++ Algorithms/BitMasking/Turn Off First Set Bit/solution.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int turnOffFirstSetBit(int n){
+    return n-(n&-n);
+}
+int main() {
+	int n;
+	cin >> n;
+	cout<< turnOffFirstSetBit(n) <<endl;
+	return 0;
+}
+
