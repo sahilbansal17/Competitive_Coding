90162873de8bcafccb18992d237a351b56844889
bag-PacKer <35328339+bag-PacKer@users.noreply.github.com>
2019-10-09 19:30:10 +0530

    Update sparsh_table.cpp
    Add comment to understand code.

--- Templates/sparsh_table.cpp
+++ Templates/sparsh_table.cpp
@@ -16,29 +16,33 @@ int Arr[N];
 int main()
 {
     int n, L, R, q;
-    cin >> n; // array size
+    cin >> n; // Input array size where we need to perform range query
     for(int i = 0; i < n; i++)
         cin >> Arr[i];
 
     // build Sparse Table
     for(int i = 0; i < n; i++)
-        table[i][0] = Arr[i];
+        table[i][0] = Arr[i]; // basic stap
+    //Function is associative: a + (b + c) = (a + b) + c
     for(int j = 1; j <= k; j++) {
+        // table[i][j] has sum of element Arr[i]+Arr[i+1]+ ... + Arr[i+2^j - 1]
         for(int i = 0; i <= n - (1 << j); i++)
-            table[i][j] = table[i][j - 1] + table[i + (1 << (j - 1))][j - 1];
+            table[i][j] = table[i][j - 1] + table[i + (1 << (j - 1))][j - 1]; // Sum is associative, so this operation is meaningful
+        //Instade of sum there can be any operation which is Associative.
     }
 
     cin >> q; // number of queries
     for(int i = 0; i < q; i++) {
         cin >> L >> R; // boundaries of next query, 0-indexed
-        long long answer = ZERO;
+        long long answer = ZERO;//ZERO is neutral element for calculating sum, this is depend on function
+        // took the binary representation to cover L to R range
         for(int j = k; j >= 0; j--) {
             if(L + (1 << j) - 1 <= R) {
                 answer = answer + table[L][j];
                 L += 1 << j; // instead of having L', we increment L directly
             }
         }
-        cout << answer << endl;
+        cout << answer << endl; // Final answer
     }
     return 0;
 }
