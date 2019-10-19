1be1b37e956f78c81769b9c1ce5992ff36092985
Anurag Patro <anurag.patro7@gmail.com>
2019-10-19 20:27:54 +0530

    Update marm.cpp

--- Problem Solutions/CodeChef/OCT19B/marm.cpp
+++ Problem Solutions/CodeChef/OCT19B/marm.cpp
@@ -2,9 +2,7 @@
 #include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
-ll mod(ll x, ll y){
-	return x-y*((ll)(x/y));
-}
+
 int main(){
 	ll t,n,k;
 	cin>>t;
@@ -14,15 +12,16 @@ int main(){
 		for(ll i=0;i<n;i++){
 			cin>>v[i];
 		}		
+		// Pattern noticed is repetition of same sequence after 3*n numbers, can be reduced
 		if(k>(3*n)){
 			if(n&1){
-				v[n/2] = 0;
+				v[n/2] = 0; // always 0, self XOR
 			}
 			k = k%(3*n);
 		}
 		for(int i=0;i<k;i++){
 			ll val = i%n;
-			v[val] = v[val] ^ v[n-1 - val];
+			v[val] = v[val] ^ v[n-1 - val]; 
 		}
 		for(ll i=0;i<n;i++){
 			cout<<v[i]<<" ";
