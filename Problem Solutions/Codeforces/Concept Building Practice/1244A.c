a5391ad5e8cdb661dfeec1107e7e6f8a3affc4b9
Pratyush Pratap Priyadarshi <ppratap2001@gmail.com>
2019-10-19 14:41:41 +0530

    Adding solution to the problem 1244A

--- Problem Solutions/Codeforces/Concept Building Practice/1244A.c
+++ Problem Solutions/Codeforces/Concept Building Practice/1244A.c
@@ -1,46 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int t,n,i,j,k,count,ans1,ans2,answer;
-    //char s[1001];
+    //pens is the number of pens required
+    //p is the number of pencils required
+    int t,a,b,c,d,k,pens,p;
+    //t is the number of test cases.
     scanf("%d",&t);
     while(t--)
     {
-        i=0;k=0;answer=0;
-        scanf("%d",&n);
-        j=n-1;count=0;
-        char s[n];
-        scanf("%s",s);
-        while(k<n)
-        {
-            if(s[k]=='1')
-            {
-                answer=1;
-                break;
-            }
-            k++;
-        }
-        while(s[i]!='1' && i<n)
-            i++;
-        while(s[j]!='1' && j>=0)
-        {
-            j--;
-            count++;
-        }
-
-        //printf("i:%d count:%d",i,count);
-        i++;count++;
-        ans1=n-i+1;
-        ans2=n-count+1;
-        if(answer==0)
-            printf("%d\n",n);
+        //scanning a,b,c,d,k
+        scanf("%d%d%d%d%d",&a,&b,&c,&d,&k);
+        //if a is exactly divisble by c then we would need only c number of pens
+        //else we would need 1 more than a/c pens
+        if(a%c==0)
+            pens=a/c;
         else
-        {
-            if(ans2>ans1)
-                printf("%d\n",2*ans2);
-            else printf("%d\n",2*ans1);
-        }
+            pens=a/c+1;
+        //similarly we can find the minimum number of pencils required 
+        if(b%d==0)
+            p=b/d;
+        else
+            p=b/d+1;
+        //checking if number of pens and pencils are less than or equal to
+        //the number of writing implements that can fit into Polycarp's pencilcase
+        if(p+pens<=k)
+            printf("%d %d",pens,p);
+        else
+            printf("-1");
+        printf("\n");
     }
     return 0;
 }
