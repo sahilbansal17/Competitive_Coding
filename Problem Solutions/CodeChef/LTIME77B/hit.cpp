8af4fd4d2734216cf93ca0e3361c42da80561de3
TANISHQ CHAMOLA <36632821+tanishqchamola@users.noreply.github.com>
2019-10-31 19:17:00 +0530

    Add files via upload

---
+++ Problem Solutions/CodeChef/LTIME77B/hit.cpp
@@ -0,0 +1,66 @@
+//
+//  hit.cpp
+//  codechef
+//
+//  Created by Tanishq Chamola on 26/10/19.
+//  Copyright © 2019 Tanishq Chamola. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n,x,y,z;
+        cin>>n;
+        int arr[n];
+        
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        
+        int partition = n/4;
+        
+        sort(arr,arr+n);
+        
+        if(arr[(3*partition)-1]!=arr[(3*partition)])
+        {
+            z=arr[(3*partition)];
+        }
+        else{
+            cout<<-1<<endl;
+            continue;
+        }
+        if(arr[(2*partition)-1]!=arr[(2*partition)])
+        {
+            y=arr[(2*partition)];
+        }
+        else{
+            cout<<-1<<endl;
+            continue;
+        }
+        if(arr[partition-1]!=arr[partition])
+        {
+            x=arr[partition];
+        }
+        else{
+            cout<<-1<<endl;
+            continue;
+        }
+        
+        
+        
+        cout<<x<<" "<<y<<" "<<z<<endl;
+        
+    }
+    
+    return 0;
+}
+*/
