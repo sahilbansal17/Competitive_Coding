7157cb4c1217c12e55f4b586b5e987f3222ff983
TANISHQ CHAMOLA <36632821+tanishqchamola@users.noreply.github.com>
2019-10-31 19:10:17 +0530

    Add files via upload

---
+++ Problem Solutions/CodeChef/OCT19B/s10e.cpp
@@ -0,0 +1,43 @@
+//
+//  s10e.cpp
+//  codechef
+//
+//  Created by Tanishq Chamola on 06/10/19.
+//  Copyright © 2019 Tanishq Chamola. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+/*
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while (t--) {
+        
+        int n,count=0;
+        cin>>n;
+        int arr[n+5];
+        
+        arr[0]=900;arr[1]=900;arr[2]=900;arr[3]=900;arr[4]=900;
+        
+        for (int i=5; i<n+5; i++) {
+            cin>>arr[i];
+        }
+        
+        for (int i=n+4; i>=5; i--) {
+            if (arr[i]>=arr[i-1]||arr[i]>=arr[i-2]||arr[i]>=arr[i-3]||arr[i]>=arr[i-4]||arr[i]>=arr[i-5]) {
+                
+            }
+            else {
+                count++;
+            }
+        }
+        
+        cout<<count<<endl;
+    }
+    
+    return 0;
+}
+*/
