eca5fc74ca5dbdf902e2fb5f9881329d68377bcb
Sahil <shlbnsl843@gmail.com>
2020-11-12 17:22:58 +0530

    Add printGrid method in grid_helper

--- cpp/grid_helper.cpp
+++ cpp/grid_helper.cpp
@@ -11,6 +11,24 @@ inline void readGrid(vector<vector<T>>& grid, int R, int C) {
   }
 }
 
+template <typename T>
+inline void printGrid(const vector<vector<T>>& grid) {
+  int R = grid.size();
+  if (R == 0) {
+    return;
+  }
+  int C = grid[0].size();
+  if (C == 0) {
+    return;
+  }
+  for (int r = 0; r < R; ++r) {
+    for (int c = 0; c < C; ++c) {
+      cout << grid[r][c] << " ";
+    }
+    cout << endl;
+  }
+}
+
 inline bool safeChecker(int x, int y, int R, int C) {
   if (x < 0 || y < 0 || x >= R || y >= C) {
     return false;
